be410f57ea4a62a84d4adc1fdad7d2633fb7e02f
myde-developer <myde1323@gmail.com>
2025-11-19 11:52:32 +0100

    Please Work Now

--- server/app.js
+++ server/app.js
@@ -12,11 +12,11 @@ app.use(express.json());
 app.use("/api/menu", require("./routes/menu"));
 app.use("/api/feedbacks", require("./routes/feedback"));
 app.use("/api/auth", require("./routes/auth"));
+app.use("/api/orders", require("./routes/orders"));
 app.use("/api/category", require("./routes/category"))
 
 // ADMIN ROUTES
 const { verifyToken } = require("./middleware/auth");
-app.use("/api/orders", verifyToken, require("./routes/orders"));
 app.use("/api/menu", verifyToken, require("./routes/menu")); 
 
 const PORT = process.env.PORT || 3000;
